0fd4204feceb4c7b884c23ac79f2ba1e77a71c5b
Wawira-Muthoni <christinewawiramuthoni@gmail.com>
2022-02-13 11:23:52 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main (){
+    var name= "Jane Ann"
+    println(name)
+    var age= 20
+    println(age)
+    var phoneNumber= +254736445889
+    println(phoneNumber)
+    var weight:String = "73kg"
+    println(weight)
+    val iskenyan: Boolean= true
+    val isnotkenyan: Boolean= false
+    println(iskenyan)
+
+
+
+}
